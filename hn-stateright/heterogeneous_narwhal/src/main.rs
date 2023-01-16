4aa07d3716256dd9ffc41b4fcbcb29859e7cd041
Tobias Heindel <tobias@heliax.dev>
2023-01-16 15:00:40 +0100

    modified:   src/main.rs client behaviour

--- hn-stateright/heterogeneous_narwhal/src/main.rs
+++ hn-stateright/heterogeneous_narwhal/src/main.rs
@@ -1,4 +1,4 @@
-// `main.rs` of Heterogeneous Narwhal
+// `main.rs` of Heterogeneousp Narwhal
 
 // all about actors from stateright
 use stateright::actor::{*};
@@ -11,17 +11,31 @@ use std::borrow::Cow;
 // use std::net::{SocketAddrV4, Ipv4Addr};
 
 // the learner graph trait uses
-use std::collections::{HashMap,HashSet};
+use std::collections::{HashMap, HashSet};
 use std::iter::Iterator;
 
-// Dummy Message Kind
-#[derive(Clone,Debug,Eq,PartialEq,Hash)]
-struct DummyMessageType; // PLACEHOLDER
+// the type of transactions, sent by the client
+type TxData = u64;
+
+// the IDs of validators
+type ValidatorId = Id;
+
+// the IDs of workers
+type WorkerId = Id;
+
+// the IDs of workers
+type ClientId = Id;
+
+// the indices of workers (globally fixed, for all validators)
+type WorkerIndex = u64;
 
 // the enumeration of all possible message types
 #[derive(Clone,Debug,Eq,PartialEq,Hash)]
 enum MessageEnum {
-    SomeKindOfMessage(DummyMessageType), // PLACEHOLDER
+    // submissions of transactions by the client/user 
+    SubmitTx(TxData, ClientId), 
+    // acknowledgments of transactions by the worker 
+    TxAck(TxData, WorkerId),
 }
 
 use crate::MessageEnum::*;
@@ -29,13 +43,14 @@ use crate::MessageEnum::*;
 // the state type of workers
 #[derive(Clone,Debug,Eq,PartialEq,Hash)]
 struct WorkerState;
+
 // the state type of primaries
 #[derive(Clone,Debug,Eq,PartialEq,Hash)]
 struct PrimaryState;
 
-// the state type of clients
-#[derive(Clone,Debug,Eq,PartialEq,Hash)]
-struct ClientState;
+// the state type of clients 
+// it is the number of requests 
+type ClientState = Vec<WorkerId>;
 
 // states can be either of a worker or a primary
 #[derive(Clone,Debug,Eq,PartialEq,Hash)]
@@ -64,26 +79,75 @@ trait LearnerGraph{
 }
 
 
-type ValidatorID = u64;
-
-type WorkerIndex = u64;
-
 #[derive(Clone)]
 struct WorkerActor{
     // the index of a worker
     index : WorkerIndex,
-    primary : ValidatorID,
+    // the primary
+    primary : ValidatorId,
+    // the ids of workers of the same index, aka mirro workers
+    mirror_workers : Vec<Id>,
 }
 
 
 #[derive(Clone)]
 struct PrimaryActor{
     // some immutable information
-    id : ValidatorID, 
+    id : ValidatorId,
+    //
+    peer_validators : Vec<Id>,
+}
+
+
+#[derive(Clone,Debug,Eq,PartialEq,Hash)]
+struct ClientActor{
+    // some immutable information
+    /// seed : Option<u64>,
+    // initial request number
+    /// initial_requests : u64,
+    // function for new requests, optionally pseudo-random
+    new_req : fn(u64) -> u64,
+    // the vector of workers to which requests can/will be sent
+    known_workers : Vec<WorkerId>,
+}
+
+// the client actor
+// - sends new requests, ideally constantly and uniformly
+impl Actor for ClientActor {
+
+    type Msg = MessageEnum;
+    type State = StateEnum;
+
+    fn on_start(&self, id: Id, o: &mut Out<Self>) -> Self::State {
+        // send a tx to every work
+        let mut x = 0;
+        for k in &self.known_workers{
+            x = x+1; 
+            o.send(*k, SubmitTx(x, id));
+        }
+        // the known workers are the only state of the client
+        // ... so far 
+        Client(self.known_workers.clone()) 
+    }
+
+    fn on_msg(&self, id: Id, _state: &mut Cow<Self::State>,
+              src: Id, msg: Self::Msg, o: &mut Out<Self>) {
+        match msg {
+            TxAck(tx, worker) => {
+                let new_tx = // this should be "random", but ... 
+                    if tx%2 == 0 {
+                        tx / 2 
+                    } else {
+                        3*tx + 1
+                    };
+                o.send(worker, SubmitTx(new_tx, id));
+            },
+            _ => {}
+        }
+    }
 }
 
 impl Actor for WorkerActor {
-    
     type Msg = MessageEnum;
     type State = StateEnum;
 
@@ -95,28 +159,27 @@ impl Actor for WorkerActor {
               src: Id, msg: Self::Msg, o: &mut Out<Self>) {
         match msg {
             _ => { 
-                o.send(src, SomeKindOfMessage(DummyMessageType{}));
+                // o.send(src, SomeKindOfMessage(DummyMessageType{}));
             }
         }
     }
 }
 
 impl Actor for PrimaryActor {
-    
     type Msg = MessageEnum;
     type State = StateEnum;
 
-    fn on_start(&self, _id: Id, _o: &mut Out<Self>) -> Self::State {
-        Primary(PrimaryState{}) // default value for one ping pongk
-    }
+    fn on_start(&self, _id: Id, _o: &mut Out<Self>) ->
+        Self::State {
+            Primary(PrimaryState{}) // default value for one ping pongk
+        }
 
     fn on_msg(&self, _id: Id, _state: &mut Cow<Self::State>,
               src: Id, msg: Self::Msg, o: &mut Out<Self>) {
         match msg {
             _ => { 
-                o.send(src, SomeKindOfMessage(DummyMessageType{}));
+                //o.send(src, SomeKindOfMessage(DummyMessageType{}));
             }
         }
     }
 }
-
